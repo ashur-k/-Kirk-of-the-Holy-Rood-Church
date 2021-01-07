583c0bb6d5dd8361680f5704a8b07e6629c6d367
ashur-k <ashurkanwal@yahoo.com>
2021-01-07 13:19:20 +0000

    added new scret key to site and protected it in ENV_VARS

--- Kirk_of_the_Holy_Rood_Church/settings.py
+++ Kirk_of_the_Holy_Rood_Church/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_d_bwexvbh+9+o_9=$ob9*v-_0vnr!@f11=_v^fv3pl@6wi)#j'
+SECRET_KEY = os.environ.get('KIRK_CHURCH_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
