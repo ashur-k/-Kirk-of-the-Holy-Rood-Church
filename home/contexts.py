3571a0718572c9b58a22082328a299a5e00f24a2
ashur-k <ashurkanwal@yahoo.com>
2021-01-09 19:16:12 +0000

    updated home page

--- home/contexts.py
+++ home/contexts.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import CurrentColorTheme
 from .forms import CurrentColorThemeForm
+from django.contrib import messages
 
 
 def site_settings(request):
@@ -9,8 +10,9 @@ def site_settings(request):
         form = CurrentColorThemeForm(request.POST, instance=color)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Successfully updated theme.')
 
-    form = CurrentColorThemeForm()
+    form = CurrentColorThemeForm(instance=color)
 
     context = {
         'color': color,
