7c1af1cbf22aeb512934feae93f6a4847f574c52
ashur-k <ashurkanwal@yahoo.com>
2021-04-20 09:00:39 +0000

    added is_free_event boolean field to event models

--- events/models.py
+++ events/models.py
@@ -10,6 +10,7 @@ class Events(models.Model):
     image = models.ImageField(blank=False, upload_to='media/')
     event_description = models.TextField()
     event_price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_free_event = models.BooleanField(default=False)
     event_ticket_quantity = models.IntegerField(default=1)
     event_remaining_ticket = models.IntegerField(default=0)
     event_display_status = models.CharField(max_length=10, choices=STATUS)
