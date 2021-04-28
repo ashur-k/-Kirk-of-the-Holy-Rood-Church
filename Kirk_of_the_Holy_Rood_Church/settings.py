946c2cd92c1c1a6179e1e3141e6714a8e4a925ff
ashur-k <ashurkanwal@yahoo.com>
2021-04-28 11:09:32 +0000

    Deploying 1

--- Kirk_of_the_Holy_Rood_Church/settings.py
+++ Kirk_of_the_Holy_Rood_Church/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('KIRK_CHURCH_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kirk-of-the-holy-rood-church.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -142,15 +142,19 @@ WSGI_APPLICATION = 'Kirk_of_the_Holy_Rood_Church.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'KIRK_DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('KIRK_DATABASE_URL'))
     }
-}
-
-
-
+    print("Postgres")
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+    print("DBSQLITE 3")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Kirk_of_the_Holy_Rood_Church.wsgi:application
