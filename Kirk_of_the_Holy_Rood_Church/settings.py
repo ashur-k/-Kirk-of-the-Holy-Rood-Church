0e1057c045bbf3bb721e3cc7ce124cee9ad4485b
ashur-k <ashurkanwal@yahoo.com>
2021-04-28 11:27:32 +0000

    Deploying 2

--- Kirk_of_the_Holy_Rood_Church/settings.py
+++ Kirk_of_the_Holy_Rood_Church/settings.py
@@ -142,9 +142,9 @@ WSGI_APPLICATION = 'Kirk_of_the_Holy_Rood_Church.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'KIRK_DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('KIRK_DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
     print("Postgres")
 else:
