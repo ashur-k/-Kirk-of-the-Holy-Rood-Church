3297e9759428a57f06c962a3006f519a7805fab7
ashur-k <ashurkanwal@yahoo.com>
2021-04-28 12:23:56 +0000

    deploying

--- Kirk_of_the_Holy_Rood_Church/settings.py
+++ Kirk_of_the_Holy_Rood_Church/settings.py
@@ -146,7 +146,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    print("Postgres")
+
 else:
     DATABASES = {
         'default': {
@@ -154,7 +154,7 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
             }
         }
-    print("DBSQLITE 3")
+   
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
